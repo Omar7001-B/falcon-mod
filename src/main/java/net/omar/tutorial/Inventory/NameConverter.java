ae477f0bc6b896bdc3ca74cc37ad6c09f9447719
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-09-01 03:57:11 +0300

    Temp class to check if this item is stackable, also to conver names, and search names.

--- src/main/java/net/omar/tutorial/Inventory/NameConverter.java
+++ src/main/java/net/omar/tutorial/Inventory/NameConverter.java
@@ -6,54 +6,40 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import static net.omar.tutorial.Tutorial.LOGGER;
+
 public class NameConverter {
 
     private static final Map<String, String> wordsMap = new HashMap<>();
+    private static final String[] STACKABLE_KEYWORDS = {"sword", "pickaxe", "axe", "helmet", "chestplate", "leggings", "boots"};
 
     static {
         wordsMap.put("ᴄᴏᴍᴘʀᴇssᴇᴅ ʀᴀᴡ ɢᴏʟᴅ", "Raw Gold");
         wordsMap.put("ᴄᴏᴍᴘʀᴇssᴇᴅ ɢᴏʟᴅ ɪɴɢᴏᴛ", "Gold Ingot");
         wordsMap.put("ᴄᴏᴍᴘʀᴇssᴇᴅ ɢᴏʟᴅ ʙʟᴏᴄᴋ", "Gold Block");
         wordsMap.put("ᴄᴏᴍᴘʀᴇssᴇᴅ ɢᴏʟᴅ ɴᴜɢɢᴇᴛ", "Gold Nugget");
-        /*
-        wordsMap.put("ɴᴜɢɢᴇᴛ", "Nugget");
-        wordsMap.put("ᴄᴏᴍᴘʀᴇssᴇᴅ ", "");
-        wordsMap.put("Shulker Box", "Box");
-        wordsMap.put("Golden", "Gold");
-        wordsMap.put("ʀᴀᴡ", "Raw");
-        wordsMap.put("ɢᴏʟᴅ", "Gold");
-        wordsMap.put("ʙʟᴏᴄᴋ", "Block");
-        wordsMap.put("ɪɴɢᴏᴛ", "Ingot");
-        wordsMap.put("sʜᴜʟᴋᴇʀ", "Shulker");
-        wordsMap.put("sᴛᴏɴᴇ", "Stone");
-        wordsMap.put("sᴡᴏʀᴅ", "Sword");
-        wordsMap.put("ᴘɪᴄᴋᴀxᴇ", "Pickaxe");
-        wordsMap.put("ᴀxᴇ", "Axe");
-        wordsMap.put("ɪʀᴏɴ", "Iron");
-        wordsMap.put("ᴀʀᴍᴏʀ", "Armor");
-        wordsMap.put("ɴᴇᴛʜᴇʀɪᴛᴇ", "Netherite");
-        wordsMap.put("ʟᴇᴀᴛʜᴇʀ", "Leather");
-        wordsMap.put("ғᴏᴏᴅ", "Food");
-        wordsMap.put("ᴄᴏᴍᴘʀᴇss", "Compressed");
-        wordsMap.put("ᴅᴇᴄᴏᴍᴘʀᴇss", "Decompressed");
-        wordsMap.put("ᴅɪᴀᴍᴏɴᴅ", "Diamond");
-        wordsMap.put("ᴇʟʏᴛʀᴀ", "Elytra");
-        wordsMap.put("ᴇʟʏᴛʀᴀ_java", "Elytra Java");
-        wordsMap.put("ᴘᴏᴛɪᴏɴ", "Potion");
-        wordsMap.put("ᴘᴠᴘ", "PVP");
-        wordsMap.put("ᴜᴛɪʟɪᴛɪᴇ", "Utility");
-        wordsMap.put("ᴡᴏᴏᴅᴇɴ", "Wooden");
-         */
+        // Add other mappings here...
     }
 
     public static String offerNamesToInventoryNames(String input) {
-        DEBUG.Shop("Converting: " + input);
+        //LOGGER.info("Converting: " + input);
         for (Map.Entry<String, String> entry : wordsMap.entrySet()) {
             if(input.contains(entry.getKey())) {
-                DEBUG.Shop("Converted: " + entry.getValue());
+                //LOGGER.info("Converted: " + entry.getValue());
                 return entry.getValue();
             }
         }
         return input;
+    }
+
+    public static boolean isStackedItem(String name) {
+        //LOGGER.info("Checking if " + name + " is stackable");
+        for (String keyword : STACKABLE_KEYWORDS) {
+            if (name.toLowerCase().contains(keyword)) {
+                //LOGGER.info("Yes, " + name + " is stackable");
+                return false;
+            }
+        }
+        return true;
     }
 }
