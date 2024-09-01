ece8281cfe3677b14cab59bb5ddf66ca1114322c
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-09-01 03:55:47 +0300

    Classes to save last state of the open Shulker/pv/Inventory

--- src/main/java/net/omar/tutorial/last/LastPV.java
+++ src/main/java/net/omar/tutorial/last/LastPV.java
@@ -1,33 +1,34 @@
 package net.omar.tutorial.last;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.screen.ingame.ShulkerBoxScreen;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.util.collection.DefaultedList;
+import net.omar.tutorial.Inventory.SlotOperations;
 import net.omar.tutorial.classes.DEBUG;
 import net.omar.tutorial.indexes.Indexes;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class LastShulker {
+public class LastPV {
 
     public static int filledSlots;
     public static int emptySlots;
     public static Map<String, Integer> itemCounts = new HashMap<>();
 
-    public static void updateShulkerData(String operation) {
+    public static void updatePVData(String operation) {
         MinecraftClient client = MinecraftClient.getInstance();
         if(client.currentScreen == null) return;
-        if(!(client.currentScreen instanceof ShulkerBoxScreen)) { resetShulkerData(); return; }
+        if(!(client.currentScreen instanceof GenericContainerScreen)) { resetPVData(); return; }
         DEBUG.Store("Operation: " + operation);
         DefaultedList<Slot> slots = ((HandledScreen<?>) client.currentScreen).getScreenHandler().slots;
         filledSlots = 0;
         emptySlots = 0;
         itemCounts.clear();
 
-        Indexes.Shulker.SHULKER_BOX.forEach(index -> {
+        Indexes.PV.PV.forEach(index -> {
             Slot slot = slots.get(index);
             if (slot.hasStack()) {
                 filledSlots++;
@@ -37,20 +38,31 @@ public class LastShulker {
                 emptySlots++;
             }
         });
-        LastInventory.updateFromShulker(operation  + " - LastShulker");
+        LastInventory.updateFromPV(operation  + " - LastPV");
     }
 
-    public static void resetShulkerData() {
+    public static void resetPVData() {
         filledSlots = 0;
         emptySlots = 0;
         itemCounts.clear();
     }
 
-    public static void showShulkerData() {
-        DEBUG.Store("Data about the Shulker:");
+    public static void showPVData() {
+        DEBUG.Store("Data about the Player's PV:");
         DEBUG.Store("Filled Slots: " + filledSlots);
         DEBUG.Store("Empty Slots: " + emptySlots);
         itemCounts.forEach((item, count) -> DEBUG.Store("Item: " + item + ", Count: " + count));
         DEBUG.Store(("-------------------"));
+    }
+
+    public static int getItemCountByName(String searchStr) {
+        DEBUG.Shop("Searching for: " + searchStr);
+        for (Map.Entry<String, Integer> entry : itemCounts.entrySet()) {
+            if (SlotOperations.containsIgnoreCase(entry.getKey(), searchStr)) {
+                DEBUG.Shop("Found: " + entry.getKey() + ", Count: " + entry.getValue());
+                return entry.getValue();
+            }
+        }
+        return 0;
     }
 }
