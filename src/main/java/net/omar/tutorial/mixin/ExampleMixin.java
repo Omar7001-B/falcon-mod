76475a4ee54644fa3b01a38aa05af1017be98c23
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-09-04 01:48:41 +0300

    Function MoveComplete is used, to allow user to click on items and buy fast.

--- src/main/java/net/omar/tutorial/mixin/ExampleMixin.java
+++ src/main/java/net/omar/tutorial/mixin/ExampleMixin.java
@@ -1,19 +1,15 @@
 package net.omar.tutorial.mixin;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.*;
-import net.minecraft.network.packet.s2c.play.SetTradeOffersS2CPacket;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.Identifier;
 import net.omar.tutorial.Inventory.SlotClicker;
 import net.omar.tutorial.Inventory.SlotOperations;
-import net.omar.tutorial.Tutorial;
+import net.omar.tutorial.Managers.TradeManager;
 import net.omar.tutorial.classes.DEBUG;
 import net.omar.tutorial.indexes.Indexes;
 import org.apache.logging.log4j.LogManager;
@@ -46,7 +42,7 @@ public class ExampleMixin {
 			SelectMerchantTradeC2SPacket tradePack = (SelectMerchantTradeC2SPacket) packet;
 			DEBUG.Mixin("Trade id is: " + tradePack.getTradeId());
 
-			if(!Tutorial.isAutomatedTrade){
+			if(!TradeManager.isAutomatedTrade){
 				int shiftKeyCode = GLFW.GLFW_KEY_LEFT_SHIFT;
 				if (InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), shiftKeyCode)) {
 					DEBUG.Mixin("Shift click on trade ID: " + tradePack.getTradeId());
@@ -54,8 +50,7 @@ public class ExampleMixin {
 				} else {
 					// Normal click on trade ID
 					DEBUG.Mixin("Normal click on trade ID: " + tradePack.getTradeId());
-					SlotClicker.slotNormalClick(Indexes.Trade.RESULT_SLOT);
-					SlotClicker.slotNormalClick(SlotOperations.getFirstEmptySlot(Indexes.Trade.TOTAL_INVENTORY));
+					SlotOperations.moveCompleteItem(Indexes.Trade.RESULT_SLOT, Indexes.Trade.TOTAL_INVENTORY);
 				}
 
 			}
