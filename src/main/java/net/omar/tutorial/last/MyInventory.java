f00cfcb7bf15d4be0b005a0e7bd6b1e0816e44c1
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-09-06 09:54:17 +0300

    Added Name

--- src/main/java/net/omar/tutorial/last/MyInventory.java
+++ src/main/java/net/omar/tutorial/last/MyInventory.java
@@ -3,6 +3,7 @@ package net.omar.tutorial.last;
 import net.omar.tutorial.indexes.Indexes;
 
 public class MyInventory extends InventoryEntry {
+    public static final String NAME = "Inventory";
     public MyInventory(String name) {
         super(name);
     }
@@ -10,17 +11,21 @@ public class MyInventory extends InventoryEntry {
     @Override
     public void update(String operation) {
         InventorySaver.updateInventoryState(name, operation, Indexes.Inventory.TOTAL_INVENTORY, this);
+        isUpdated = true;
     }
 
     public void updateFromPV() {
         InventorySaver.updateInventoryState(name, "Update from PV", Indexes.PV.TOTAL_INVENTORY, this);
+        isUpdated = true;
     }
 
     public void updateFromShulker() {
         InventorySaver.updateInventoryState(name, "Update from Shulker", Indexes.Shulker.TOTAL_INVENTORY, this);
+        isUpdated = true;
     }
 
     public void updateFromTrade() {
         InventorySaver.updateInventoryState(name, "Update from Trade", Indexes.Trade.TOTAL_INVENTORY, this);
+        isUpdated = true;
     }
 }
