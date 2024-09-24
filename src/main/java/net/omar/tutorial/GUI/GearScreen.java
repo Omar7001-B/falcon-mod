b8975a5fdc006f0dec7544d593cd57633953deab
Omar <benjami4n@gmail.com>
2024-09-24 22:55:30 +0300

    Refactored the code into files  and organized it.

--- src/main/java/net/omar/tutorial/GUI/GearScreen.java
+++ src/main/java/net/omar/tutorial/GUI/GearScreen.java
@@ -8,16 +8,15 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.text.Text;
-import net.omar.tutorial.classes.DEBUG;
-import net.omar.tutorial.classes.TreeNode;
-import net.omar.tutorial.indexes.Market;
+import net.omar.tutorial.Managers.Trading;
+import net.omar.tutorial.Managers.Debugging;
+import net.omar.tutorial.classes.Shopper;
+import net.omar.tutorial.Data.Market;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-
-import static net.omar.tutorial.Tutorial.*;
 
 @Environment(EnvType.CLIENT)
 public class GearScreen extends Screen {
@@ -32,6 +31,8 @@ public class GearScreen extends Screen {
     private final SimpleOption<Integer> SwordSlider = createSliderOption("Sword", 0, 7);
     private final SimpleOption<Integer> PickAxeSlider = createSliderOption("PickAxe", 0, 7);
     private final SimpleOption<Integer> AxeSlider = createSliderOption("Axe", 0, 7);
+    private final SimpleOption<Integer> BowSlider = createSliderOption("Bow", 0, 7);          // Added Bow Slider
+    private final SimpleOption<Integer> ShearsSlider = createSliderOption("Shears", 0, 7);    // Added Shears Slider
 
     public GearScreen(Screen parent) {
         super(TITLE);
@@ -42,49 +43,92 @@ public class GearScreen extends Screen {
     protected void init() {
         int padding = 10;
         int centerX = this.width / 2;
+        int startY = this.height / 6 + 24;
 
         // Adding sliders for the gear
-        this.addDrawableChild(this.ArmorSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, this.height / 6 + 24, SLIDER_WIDTH));
-        this.addDrawableChild(this.ElytraSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, this.height / 6 + 24 * 2, SLIDER_WIDTH));
-        this.addDrawableChild(this.SwordSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, this.height / 6 + 24 * 3, SLIDER_WIDTH));
-        this.addDrawableChild(this.PickAxeSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, this.height / 6 + 24 * 4, SLIDER_WIDTH));
-        this.addDrawableChild(this.AxeSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, this.height / 6 + 24 * 5, SLIDER_WIDTH));
+        this.addDrawableChild(this.ArmorSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, startY, SLIDER_WIDTH));
+        this.addDrawableChild(this.ElytraSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, startY + 24, SLIDER_WIDTH));
+        this.addDrawableChild(this.SwordSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, startY + 48, SLIDER_WIDTH));
+        this.addDrawableChild(this.PickAxeSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, startY + 72, SLIDER_WIDTH));
+        this.addDrawableChild(this.AxeSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, startY + 96, SLIDER_WIDTH));
+        this.addDrawableChild(this.BowSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, startY + 120, SLIDER_WIDTH));          // Positioned Bow Slider
+        this.addDrawableChild(this.ShearsSlider.createWidget(this.client.options, centerX - SLIDER_WIDTH / 2, startY + 144, SLIDER_WIDTH));    // Positioned Shears Slider
 
         // Back and Submit buttons
         int buttonWidth = 100;
         int buttonHeight = 20;
         int totalButtonWidth = 2 * buttonWidth + padding;
 
+        // Adjust button Y position to accommodate additional sliders
+        int buttonsY = startY + 168; // 144 + 24
+
         this.addDrawableChild(ButtonWidget.builder(Text.literal("Back"), button -> this.client.setScreen(this.parent))
-                .dimensions(centerX - totalButtonWidth / 2, this.height / 6 + 24 * 7, buttonWidth, buttonHeight).build());
+                .dimensions(centerX - totalButtonWidth / 2, buttonsY, buttonWidth, buttonHeight).build());
 
         this.addDrawableChild(ButtonWidget.builder(Text.literal("Submit"), button -> {
             close();
 
-            TreeNode elytraOnly_p2 = Market.elytra_P2.clone();
+            // Clone and modify TreeNodes as per original logic
+            Shopper elytraOnly_p2 = Market.elytra_P2.clone();
             elytraOnly_p2.trades.remove(0);
             elytraOnly_p2.trades.remove(0);
 
-            DEBUG.Shulker("Armor" + elytraOnly_p2);
+            Shopper bowOnly_p2 = Market.pvpUtilitys_P2.clone();
+            bowOnly_p2.trades = new ArrayList<>();
+            bowOnly_p2.trades.add(Market.goldIngotToBowVI_t);
+            bowOnly_p2.trades.add(Market.goldIngotToBowVII_t);
 
-            int[] values = {this.ArmorSlider.getValue(), this.ElytraSlider.getValue(), this.SwordSlider.getValue(), this.PickAxeSlider.getValue(), this.AxeSlider.getValue()};
-            String[] items = {"Armor", "Elytra", "Sword", "PickAxe", "Axe"};
-            List<TreeNode> nodes = List.of(Market.armors_P1, elytraOnly_p2, Market.swords_P1, Market.pickaxes_P1, Market.axes_P1);
+            Shopper shearsOnly_p2 = Market.pvpUtilitys_P2.clone();
+            shearsOnly_p2.trades = new ArrayList<>();
+            shearsOnly_p2.trades.add(Market.rawgoldToShearsIII_t);
+
+            Debugging.Shulker("Armor" + elytraOnly_p2);
+
+            // Include Bow and Shears in the values array
+            int[] values = {
+                    this.ArmorSlider.getValue(),
+                    this.ElytraSlider.getValue(),
+                    this.SwordSlider.getValue(),
+                    this.PickAxeSlider.getValue(),
+                    this.AxeSlider.getValue(),
+                    this.BowSlider.getValue(),          // Bow value
+                    this.ShearsSlider.getValue()        // Shears value
+            };
+
+            String[] items = {
+                    "Armor",
+                    "Elytra",
+                    "Sword",
+                    "PickAxe",
+                    "Axe",
+                    "Bow",          // Bow
+                    "Shears"        // Shears
+            };
+
+            // Include Bow and Shears TreeNodes
+            List<Shopper> nodes = List.of(
+                    Market.armors_P1,
+                    elytraOnly_p2,
+                    Market.swords_P1,
+                    Market.pickaxes_P1,
+                    Market.axes_P1,
+                    bowOnly_p2,     // Bow TreeNode
+                    shearsOnly_p2   // Shears TreeNode
+            );
 
             CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS).execute(() -> {
                 for (int i = 0; i < values.length; i++) {
                     int value = values[i];
-                    if(value == 0) {
+                    if (value == 0) {
                         continue;
-                    } else
-                    if (value <= 6) {
-                        getMaterialAndBuyItem(nodes.get(i), value);
+                    } else if (value <= 6) {
+                        Trading.getMaterialAndBuyItem(nodes.get(i), value);
                     } else {
-                        getMaterialAndBuyItem(nodes.get(i), 9999);
+                        Trading.getMaterialAndBuyItem(nodes.get(i), 9999);
                     }
                 }
             });
-        }).dimensions(centerX + padding / 2, this.height / 6 + 24 * 7, buttonWidth, buttonHeight).build());
+        }).dimensions(centerX + padding / 2, buttonsY, buttonWidth, buttonHeight).build());
     }
 
     private static SimpleOption<Integer> createSliderOption(String label, int min, int max) {
